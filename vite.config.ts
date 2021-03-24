4c02e0fea3c7f5dcce62ee22af920cade4ac5ab7
lanyunqing <439891354@qq.com>
2021-03-24 11:40:34 +0800

    Init

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import vue from '@vitejs/plugin-vue';
+import path from "path";
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [vue()]
+})
